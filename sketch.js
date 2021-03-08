077a9f52e2614cc85dc43da470067d8a0e380128
Devilkabby <77492157+Devilkabby@users.noreply.github.com>
2021-03-08 18:06:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,79 +1,35 @@
-// Namespacing - nicknames
-
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+const Body = Matter.Body;
 
-var engine, world;
-var box1, pig1;
-var backgroundImg;
-var platform;
-var log6;
-var rubber;
-
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+function preload()
+{
+	
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+function setup() {
+	createCanvas(800, 700);
 
-    
-    ground = new Ground(600,height,1200,20);
-   
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+	engine = Engine.create();
+	world = engine.world;
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+	//Create the Bodies Here.
 
-    log3 =  new Log(810,180,300, PI/2);
 
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-    log6 = new Log(230,180,80,PI/2)
-
-    bird = new Bird(100,100);
-
-    platform = new Ground(150,305,300,170);
-
-    rubber = new Slingshot(bird.body,log6.body)
+	Engine.run(engine);
+  
 }
 
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
 
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-    log6.display();
-    rubber.display();
-     
-    
-
-    bird.display();
-    platform.display();
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
 }
+
+
+
